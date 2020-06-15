939c53f1c75bb2dfb7a7cf3f946abc015b790367
Matt Styles <matt@personalurban.com>
2020-06-15 11:49:21 +0100

    add map objects, arrays, and strings

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,10 +1,6 @@
 
-// const tape = require('ava')
-// const { stringToHsl } = require('../lib')
-
-import test from 'ava'
-
-import { stringToHsl } from '../lib/index.js'
+const test = require('ava')
+const { stringToHsl, objectToHsl } = require('../lib')
 
 test('It should convert various colour representations to an hsl array', t => {
   t.plan(4)
@@ -13,4 +9,18 @@ test('It should convert various colour representations to an hsl array', t => {
   t.deepEqual(stringToHsl('#fff'), 'hsl(0,0%,100%)', '3 digit hex to hsl')
   t.deepEqual(stringToHsl('rgb(0, 0, 0)'), 'hsl(0,0%,0%)', 'rgb to hsl')
   t.deepEqual(stringToHsl('rebeccapurple'), 'hsl(270,50%,40%)', 'named to hsl')
+})
+
+test('It should convert objects and arrays to have colour values in hsl', t => {
+  const tests = [
+    ['flat', { rose: '#ea0064' }, { rose: 'hsl(334.36,100%,45.88%)' }],
+    ['deep', { deep: { rose: '#ea0064' } }, { deep: { rose: 'hsl(334.36,100%,45.88%)' } }],
+    ['array', ['#ea0064'], ['hsl(334.36,100%,45.88%)']]
+  ]
+
+  t.plan(tests.length)
+
+  tests.forEach(([id, fixture, expected]) => {
+    t.deepEqual(objectToHsl(fixture), expected, `${id}`)
+  })
 })
